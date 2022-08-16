b80f1c344711c60d30c5a4ac4ab5aec18e2644fb
Raimar Silva <raimars@hotmail.com>
2022-08-16 14:55:03 -0300

    Subindo projeto inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto da disciplina de GCTS
+Projeto simples para a disciplina de Gerência de Configuração
+e Teste de Software.
+
+## Tecnologias
+- Java 8;
+- Spring Boot 2;
+- Spring Web;
+- Spring Data JPA;
+- Springdoc OpenAPI UI
+- PostgreSQL;
---
+++ src/test/java/com/projeto/teste/ProjetoTestes/ProjetoTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.teste.ProjetoTestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
