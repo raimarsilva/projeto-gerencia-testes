7582d02a2f78cfcf0d57f1d8dc3b273e49664a72
Raimar Silva <raimars@hotmail.com>
2022-08-18 14:37:04 -0300

    Adicionadas as dependencias do lombok nos modelos.

--- src/test/java/com/projeto/teste/ProjetoTestes/Advogado.java
+++ src/test/java/com/projeto/teste/ProjetoTestes/Advogado.java
@@ -3,57 +3,22 @@
  */
 package com.projeto.teste.ProjetoTestes;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 /**
- * @author Raimar Silva
+ * @author Iramar Jetterson Ramon Raimar
  *
  */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
 final class Advogado {
 	private int idAdv=0;
 	private String nomeAdv="";
 	private String registroOAB="";
 	private Processo processo;
-	
-	protected Advogado(int idAdv, String nomeAdv, String registroOAB, Processo processo) {
-		super();
-		this.idAdv = idAdv;
-		this.nomeAdv = nomeAdv;
-		this.registroOAB = registroOAB;
-		this.processo = processo;
-	}
-	
-	
-
-	protected int getIdAdv() {
-		return idAdv;
-	}
-
-	protected void setIdAdv(int idAdv) {
-		this.idAdv = idAdv;
-	}
-
-	protected String getNomeAdv() {
-		return nomeAdv;
-	}
-
-	protected void setNomeAdv(String nomeAdv) {
-		this.nomeAdv = nomeAdv;
-	}
-
-	protected String getRegistroOAB() {
-		return registroOAB;
-	}
-
-	protected void setRegistroOAB(String registroOAB) {
-		this.registroOAB = registroOAB;
-	}
-
-	protected Processo getProcesso() {
-		return processo;
-	}
-
-	protected void setProcesso(Processo processo) {
-		this.processo = processo;
-	}
 	
 	@Override
 	public String toString() {
--- src/test/java/com/projeto/teste/ProjetoTestes/Processo.java
+++ src/test/java/com/projeto/teste/ProjetoTestes/Processo.java
@@ -3,22 +3,22 @@
  */
 package com.projeto.teste.ProjetoTestes;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 /**
- * @author Raimar Silva de Lima
+ * @author Iramar Jetterson Ramon Raimar
  *
  */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
 final class Processo {
 	private String numProces = "";
 	private String orgaoJudic = "";
 	private String relator="";
 	private Advogado advogado;
-	
-	protected Processo(String numProces, String orgaoJudic, String relator, Advogado advogado) {
-		super();
-		this.numProces = numProces;
-		this.orgaoJudic = orgaoJudic;
-		this.relator = relator;
-	}
 	
 	protected Boolean associarAdvogado(Advogado advog) {
 		this.advogado = advog;
@@ -28,38 +28,6 @@ final class Processo {
 	protected Boolean dissociarAdvogado(Advogado advog) {
 		// fazer logica.
 		return true;
-	}
-
-	protected String getNumProces() {
-		return numProces;
-	}
-
-	protected void setNumProces(String numProces) {
-		this.numProces = numProces;
-	}
-
-	protected String getOrgaoJudic() {
-		return orgaoJudic;
-	}
-
-	protected void setOrgaoJudic(String orgaoJudic) {
-		this.orgaoJudic = orgaoJudic;
-	}
-
-	protected String getRelator() {
-		return relator;
-	}
-
-	protected void setRelator(String relator) {
-		this.relator = relator;
-	}
-
-	protected Advogado getAdvogado() {
-		return advogado;
-	}
-
-	protected void setAdvogado(Advogado advogado) {
-		this.advogado = advogado;
 	}
 
 	@Override
