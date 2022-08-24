faca01b1940544206725edc670bb90c92ebbe0f4
Raimar Silva <raimars@hotmail.com>
2022-08-24 14:47:19 -0300

    Teste de atualizar advogado

---
+++ src/test/java/com/projeto/teste/service/AdvogadoServiceTest.java
@@ -0,0 +1,94 @@
+package com.projeto.teste.service;
+
+import com.projeto.teste.ProjetoTestes.model.Advogado;
+import com.projeto.teste.ProjetoTestes.repository.AdvogadoRepository;
+import com.projeto.teste.ProjetoTestes.service.AdvogadoService;
+import com.projeto.teste.util.AdvogadoUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+public class AdvogadoServiceTest {
+
+    @InjectMocks
+    AdvogadoService advogadoService;
+
+    @Mock
+    AdvogadoRepository advogadoRepository;
+
+    @BeforeEach
+    void setup(){
+
+        // Quando for executado o findById do repository para o atualizarCliente
+        BDDMockito.when(advogadoRepository.findById(ArgumentMatchers.eq(1L)))
+                .thenReturn(Optional.ofNullable(AdvogadoUtil.advogadoValido()));
+
+        // Quando for executado o save do repository, esse é executado tanto para salvar e atualizar
+        BDDMockito.when(advogadoRepository.save(ArgumentMatchers.any()))
+                .thenReturn(AdvogadoUtil.advogadoValido());
+
+        // Quando for executado o save do repository de um advogado com processo vinculado
+        BDDMockito.when(advogadoRepository
+                        .save(ArgumentMatchers.eq(AdvogadoUtil.advogadoProcessoVinculado())))
+                .thenReturn(AdvogadoUtil.advogadoProcessoVinculado());
+    }
+
+    @Test
+    @DisplayName(value = "Atualizar um advogado com sucesso")
+    void atualizarAdvogado_quandoSucesso(){
+
+        Advogado advogadoEsperado = AdvogadoUtil.advogadoParaSalvar();
+        // Salvando um cliente
+        advogadoEsperado = advogadoService.salvar(advogadoEsperado);
+
+        // Modificando o cliente
+        advogadoEsperado.setNome("Ciclano dos Santos");
+
+        // chamando o método que será testado
+        Advogado advogadoAtualizado = advogadoService.atualizar(advogadoEsperado.getId(),advogadoEsperado);
+
+        // Verificações
+        Assertions.assertNotNull(advogadoAtualizado);
+        Assertions.assertEquals(advogadoEsperado.getId(), advogadoAtualizado.getId());
+        Assertions.assertEquals(advogadoEsperado.getNome(), advogadoAtualizado.getNome());
+        Assertions.assertEquals(advogadoEsperado.getRegistroOAB(), advogadoAtualizado.getRegistroOAB());
+
+    }
+
+    @Test
+    @DisplayName(value = "Tentar atualizar um advogado que não existe")
+    void atualizarAdvogado_SemSucesso(){
+
+        Advogado advogadoEsperado = AdvogadoUtil.advogadoParaSalvar();
+        // Salvando um cliente
+        advogadoEsperado = advogadoService.salvar(advogadoEsperado);
+
+        // Modificando o cliente
+        advogadoEsperado.setNome("Ciclano dos Santos");
+
+        // chamando o método que será testado com ID de advogado que não existe
+        Advogado advogadoAtualizado = advogadoService.atualizar(2L,advogadoEsperado);
+
+        // Verificações
+        Assertions.assertNull(advogadoAtualizado);
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            advogadoAtualizado.getId();
+            advogadoAtualizado.getNome();
+            advogadoAtualizado.getRegistroOAB();
+        });
+    }
+
+
+
+}
--- src/test/java/com/projeto/teste/util/AdvogadoUtil.java
+++ src/test/java/com/projeto/teste/util/AdvogadoUtil.java
@@ -4,7 +4,7 @@ import com.projeto.teste.ProjetoTestes.model.Advogado;
 
 public class AdvogadoUtil {
     // Retorna um advogado sem id
-    public static Advogado advogadoParaSalvar(){
+    public static Advogado advogadoParaSalvar() {
         return Advogado.builder()
                 .nome("Fulano da Silva")
                 .registroOAB("UF999999")
@@ -12,7 +12,7 @@ public class AdvogadoUtil {
     }
 
     // Retorna um advogado com id
-    public static Advogado advogadoValido(){
+    public static Advogado advogadoValido() {
 
         return Advogado.builder()
                 .id(1L)
@@ -20,4 +20,18 @@ public class AdvogadoUtil {
                 .registroOAB("UF999999")
                 .build();
     }
+
+    // Retorna um advogado com id e um processo vinculado
+    public static Advogado advogadoProcessoVinculado() {
+
+        Advogado advogado = Advogado.builder()
+                .id(1L)
+                .nome("Fulano da Silva")
+                .registroOAB("UF999999")
+                .build();
+
+        advogado.getProcessos().add(ProcessoUtil.processoValido());
+        return advogado;
+    }
+
 }
