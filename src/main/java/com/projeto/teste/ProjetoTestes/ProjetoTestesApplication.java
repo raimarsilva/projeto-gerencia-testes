c84a9f66daa048c6181628056d28d32adede88e4
Raimar Silva <raimars@hotmail.com>
2026-01-31 14:11:37 -0300

    feat: added springsec config, version displaying and updated gitignore.

--- src/main/java/com/projeto/teste/ProjetoTestes/ProjetoTestesApplication.java
+++ src/main/java/com/projeto/teste/ProjetoTestes/ProjetoTestesApplication.java
@@ -2,11 +2,13 @@ package com.projeto.teste.ProjetoTestes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.SpringSecurityCoreVersion;
 
 @SpringBootApplication
 public class ProjetoTestesApplication {
 
 	public static void main(String[] args) {
+		SpringSecurityCoreVersion.getVersion();
 		SpringApplication.run(ProjetoTestesApplication.class, args);
 	}
 
