7ee39acfed76b1491875ebdf85701711036eac26
Raimar Silva <raimars@hotmail.com>
2026-02-12 14:20:44 -0300

    chore: adjusted login order and page layout. added logout.

--- src/main/java/com/projeto/teste/ProjetoTestes/configs/MvcConfig.java
+++ src/main/java/com/projeto/teste/ProjetoTestes/configs/MvcConfig.java
@@ -8,13 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 class MvcConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addViewControllers(@NonNull ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
-		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/index").setViewName("index");
-	}
+  @Override
+  public void addViewControllers(@NonNull ViewControllerRegistry registry) {
+    registry.addViewController("/home").setViewName("home");
+    registry.addViewController("/hello").setViewName("hello");
+    registry.addViewController("/login").setViewName("login");
+  }
 
 }
