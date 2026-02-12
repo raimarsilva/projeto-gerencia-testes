283065d59acb0fd7d7439f616a0742dd87ce9eb1
Raimar Silva <raimars@hotmail.com>
2026-02-12 14:20:44 -0300

    chore: adjusted login order and page layout. added logout.

--- src/main/java/com/projeto/teste/ProjetoTestes/controller/ContratoController.java
+++ src/main/java/com/projeto/teste/ProjetoTestes/controller/ContratoController.java
@@ -10,72 +10,64 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
 //import java.util.List;
 
 @Controller
 @RequestMapping(path = "/")
 public class ContratoController {
 
-    @Autowired
-    ContratoService contratoService;
+  @Autowired
+  ContratoService contratoService;
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("contratos", contratoService.listarTodos());
-        return "index";
-    }
-    
-    // Rota para salvar
-    @PostMapping(path = "/cadastrar")
-    public String salvar(@ModelAttribute Contrato contrato){
-        contratoService.salvar(contrato);
-        return "redirect:/";
-    }
+  @GetMapping({ "", "/", "/index" })
+  public String index(Model model) {
+    model.addAttribute("contratos", contratoService.listarTodos());
+    return "index";
+  }
 
-    // Rota para listar todos
-    @GetMapping(path = "/list")
-    public List<Contrato> listarTodos(){
-      List<Contrato> contratos = contratoService.listarTodos();
-      return contratos;
-    }
-/*
-    // Rota para listar todos
-    @GetMapping(path = "/contratos")
-    public ResponseEntity<List<Contrato>> listarTodos(){
-        return ResponseEntity.status(HttpStatus.OK).body(contratoService.listarTodos());
-    }
+  // Rota para salvar
+  @PostMapping(path = "/cadastrar")
+  public String salvar(@ModelAttribute Contrato contrato) {
+    contratoService.salvar(contrato);
+    return "redirect:/";
+  }
 
-    // Rota para buscar pelo id
-    @GetMapping(path = "/contrato/{id}")
-    public ResponseEntity<Contrato> buscarPeloId(@PathVariable Long id){
-        Contrato contrato = contratoService.buscarPeloId(id);
-        if(contrato != null){
-            return ResponseEntity.status(HttpStatus.OK).body(contrato);
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(contrato);
-        }
-    }
-
-    // Rota para Deletar
-    @DeleteMapping(path = "/contrato/{id}")
-    public ResponseEntity<?> deletarPeloId(@PathVariable Long id){
-        if(contratoService.deletarPeloId(id)){
-            return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não encontrado");
-        }
-    }
-
-    // Rota para Atualizar
-    @PatchMapping(path = "/contrato/{id}")
-    public ResponseEntity<Contrato> atualizar(@PathVariable Long id, @RequestBody Contrato contrato){
-        Contrato contratoAtualizado = contratoService.atualizar(id, contrato);
-        if(contratoAtualizado != null){
-            return ResponseEntity.status(HttpStatus.OK).body(contratoAtualizado);
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(contratoAtualizado);
-        }
-    }
-*/
+  // Rota para listar todos
+  @GetMapping(path = "/list")
+  public List<Contrato> listarTodos() {
+    List<Contrato> contratos = contratoService.listarTodos();
+    return contratos;
+  }
+  /*
+   * // Rota para listar todos
+   * 
+   * @GetMapping(path = "/contratos") public ResponseEntity<List<Contrato>>
+   * listarTodos(){ return
+   * ResponseEntity.status(HttpStatus.OK).body(contratoService.listarTodos()); }
+   * 
+   * // Rota para buscar pelo id
+   * 
+   * @GetMapping(path = "/contrato/{id}") public ResponseEntity<Contrato>
+   * buscarPeloId(@PathVariable Long id){ Contrato contrato =
+   * contratoService.buscarPeloId(id); if(contrato != null){ return
+   * ResponseEntity.status(HttpStatus.OK).body(contrato); }else{ return
+   * ResponseEntity.status(HttpStatus.NOT_FOUND).body(contrato); } }
+   * 
+   * // Rota para Deletar
+   * 
+   * @DeleteMapping(path = "/contrato/{id}") public ResponseEntity<?>
+   * deletarPeloId(@PathVariable Long id){ if(contratoService.deletarPeloId(id)){
+   * return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
+   * }else{ return
+   * ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não encontrado"); } }
+   * 
+   * // Rota para Atualizar
+   * 
+   * @PatchMapping(path = "/contrato/{id}") public ResponseEntity<Contrato>
+   * atualizar(@PathVariable Long id, @RequestBody Contrato contrato){ Contrato
+   * contratoAtualizado = contratoService.atualizar(id, contrato);
+   * if(contratoAtualizado != null){ return
+   * ResponseEntity.status(HttpStatus.OK).body(contratoAtualizado); }else{ return
+   * ResponseEntity.status(HttpStatus.NOT_FOUND).body(contratoAtualizado); } }
+   */
 }
