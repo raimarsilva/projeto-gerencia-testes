5e8839453780bb964568a6483b2491056d601b30
Raimar Silva <raimars@hotmail.com>
2026-04-30 23:04:47 -0300

    chore: cleaned build.gradle, refactored service, added salary logic and fixed index view.

--- src/main/resources/static/js/remuneracao.js
+++ src/main/resources/static/js/remuneracao.js
@@ -4,19 +4,53 @@ console.log(r)
 
 function mudarSalario() {
     const ch = document.getElementById("cargaHoraria").value;
+    console.log("carga horaria: " + ch);
     const tt = document.getElementById("titulacao").value;
-    let novo = document.getElementById("remun").value;
+    console.log("titulacao: " + tt);
+    let valor;
     if (ch === "20") {
-        const valor = document.getElementById("rem" + tt).dataset.value;
+        valor = document.getElementById("rem" + tt).dataset.value;
+        console.log("valor: " + valor);
         document.getElementById("remun").innerText = valor;
-        console.log("salario mudou");
+        console.log("valor: " + valor);
     } else if (ch === "40") {
-        const valor = document.getElementById("rem" + tt).dataset.value;
+        valor = document.getElementById("rem" + (5 + parseInt(tt)).toString()).dataset.value;
         document.getElementById("remun").innerText = valor;
-        console.log("salario mudou");
+        console.log("valor: " + valor);
     } else if (ch === "99") {
-        const valor = document.getElementById("rem" + tt).dataset.value;
-        document.getElementById("remun").innerText = valor;
-        console.log("salario mudou");
+        let i = document.getElementById("cargo").selectedIndex;
+        switch (i) {
+            case 4:
+                valor = document.getElementById("rem" + (6 + parseInt(tt)).toString()).dataset.value;
+                document.getElementById("remun").innerText = valor;
+                console.log("valor: " + valor);
+                break;
+            case 5:
+                valor = document.getElementById("rem" + (7 + parseInt(tt)).toString()).dataset.value;
+                document.getElementById("remun").innerText = valor;
+                console.log("valor: " + valor);
+                break;
+            case 6:
+                valor = document.getElementById("rem" + (8 + parseInt(tt)).toString()).dataset.value;
+                document.getElementById("remun").innerText = valor;
+                console.log("valor: " + valor);
+                break;
+        }
+    }
+    document.getElementById("remInput").value = valor;
+}
+
+function mudarClasse() {
+    let classe = document.getElementById("classe");
+    switch (document.getElementById("cargo").selectedIndex) {
+        case 4: classe.value = "Adjunto";
+            break;
+        case 5: classe.value = "Associado";
+            break;
+        case 6: classe.value = "Titular";
+            break;
+        case 2: classe.value = "A";
+            break;
+        default: classe.value = "Assistente";
     }
 }
