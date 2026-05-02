dac30cc75b8b3648ea3d613b9ae197d179f94c75
Raimar Silva <raimars@hotmail.com>
2026-05-02 20:14:51 -0300

    chore: Splited listing page from insert page. Added pdf converter and html template.

--- src/main/java/com/projeto/teste/ProjetoTestes/configs/SecurityConfig.java
+++ src/main/java/com/projeto/teste/ProjetoTestes/configs/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.projeto.teste.ProjetoTestes.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,15 +22,9 @@ public class SecurityConfig {
             auth -> auth.antMatchers("/login", "/error", "/css/**").permitAll().anyRequest().authenticated())
         .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
         .logout(logout -> logout.logoutSuccessUrl("/login").deleteCookies("JSESSIONID"));
-    // .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
-    // jwt.decoder(jwtDecoder())));
 
     return http.build();
   }
-  /*
-   * @Bean public JwtDecoder jwtDecoder() { return
-   * JwtDecoders.fromIssuerLocation(issuerUri); }
-   */
 
   @Autowired
   public void configure(AuthenticationManagerBuilder auth) {
