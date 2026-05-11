f34b2348fcb2847d5b4ac8bbb81db7314242865e
Raimar Silva <raimars@hotmail.com>
2026-05-11 16:03:02 -0300

    fix: fixed csrf security config.

--- src/main/java/com/projeto/teste/ProjetoTestes/configs/SecurityConfig.java
+++ src/main/java/com/projeto/teste/ProjetoTestes/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.projeto.teste.ProjetoTestes.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +18,7 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
+    http.csrf(Customizer.withDefaults())
         .authorizeHttpRequests(
             auth -> auth.antMatchers("/login", "/error", "/css/**", "/gerar").permitAll().anyRequest().authenticated())
         .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
