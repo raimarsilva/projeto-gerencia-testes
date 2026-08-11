165e9d0e2db77924ce47be37b7c0a26091818076
Raimar Silva <raimars@hotmail.com>
2026-08-11 15:30:52 -0300

    fix: fixed salary showing. updated login page styling.

--- src/main/resources/static/js/remuneracao.js
+++ src/main/resources/static/js/remuneracao.js
@@ -1,36 +1,46 @@
-let r = document.getElementById("remun").innerText
-console.log(r)
-
 function mudarSalario() {
+    let r = document.getElementById("remun").innerText
+    console.log(r)
+    
     const ch = document.getElementById("cargaHoraria").value;
     console.log("carga horaria: " + ch);
     const tt = document.getElementById("titulacao").value;
     console.log("titulacao: " + tt);
+    let n;
+    switch(tt){
+        case "Graduado": n = 0;break;
+        case "Aperfeiçoado": n=1;break;
+        case "Especialista": n=2;break;
+        case "Mestre": n=3;break;
+        case "Doutor": n=4;break;
+        default: n=0;
+    }
+    console.log("índice: " + n)
     let valor;
     if (ch === "20") {
-        valor = document.getElementById("rem" + tt).dataset.value;
+        valor = document.getElementById("rem" + n).dataset.value;
         console.log("valor: " + valor);
         document.getElementById("remun").innerText = valor;
         console.log("valor: " + valor);
     } else if (ch === "40") {
-        valor = document.getElementById("rem" + (5 + parseInt(tt)).toString()).dataset.value;
+        valor = document.getElementById("rem" + (5 + n)).dataset.value;
         document.getElementById("remun").innerText = valor;
         console.log("valor: " + valor);
     } else if (ch === "99") {
         let i = document.getElementById("cargo").selectedIndex;
         switch (i) {
             case 4:
-                valor = document.getElementById("rem" + (6 + parseInt(tt)).toString()).dataset.value;
+                valor = document.getElementById("rem" + (6+n)).dataset.value;
                 document.getElementById("remun").innerText = valor;
                 console.log("valor: " + valor);
                 break;
             case 5:
-                valor = document.getElementById("rem" + (7 + parseInt(tt)).toString()).dataset.value;
+                valor = document.getElementById("rem" + (7+n)).dataset.value;
                 document.getElementById("remun").innerText = valor;
                 console.log("valor: " + valor);
                 break;
             case 6:
-                valor = document.getElementById("rem" + (8 + parseInt(tt)).toString()).dataset.value;
+                valor = document.getElementById("rem" + (8+n)).dataset.value;
                 document.getElementById("remun").innerText = valor;
                 console.log("valor: " + valor);
                 break;
