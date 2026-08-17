b7640d4ca4872b3271598a00285f09cd19ba33aa
Raimar Silva <raimars@hotmail.com>
2026-08-17 16:05:12 -0300

    fix: fixed logout error. Enhanced login and error page viewing.

--- src/main/java/com/projeto/teste/projetotestes/configs/SecurityConfig.java
+++ src/main/java/com/projeto/teste/projetotestes/configs/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.csrf.InvalidCsrfTokenException;
+import org.springframework.security.web.csrf.MissingCsrfTokenException;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive;
 
@@ -25,12 +27,22 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(Customizer.withDefaults()).authenticationProvider(authProvider)
         .headers(headers -> headers.cacheControl(Customizer.withDefaults()))
+        .sessionManagement(session -> session.invalidSessionUrl(LOGINURL + "?timeout=true"))
         .authorizeHttpRequests(
             auth -> auth.antMatchers(LOGINURL, "/error", "/css/**", "/*.jsf").permitAll().anyRequest().authenticated())
         .formLogin(form -> form.loginPage(LOGINURL).defaultSuccessUrl("/home", true).permitAll())
         .logout(logout -> logout.logoutSuccessUrl(LOGINURL).invalidateHttpSession(true).clearAuthentication(true)
-            .deleteCookies("JSESSIONID").addLogoutHandler(new HeaderWriterLogoutHandler(
-                new ClearSiteDataHeaderWriter(Directive.COOKIES, Directive.CACHE, Directive.STORAGE))));
+            .deleteCookies("JSESSIONID")
+            .addLogoutHandler(new HeaderWriterLogoutHandler(
+                new ClearSiteDataHeaderWriter(Directive.COOKIES, Directive.CACHE, Directive.STORAGE))))
+        .exceptionHandling(exception -> exception.accessDeniedHandler((request, response, accessDeniedException) -> {
+          if (accessDeniedException instanceof MissingCsrfTokenException
+              || accessDeniedException instanceof InvalidCsrfTokenException) {
+            response.sendRedirect("/login?expired=true");
+          } else {
+            response.sendRedirect("/error");
+          }
+        }));
 
     return http.build();
   }
